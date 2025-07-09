fd508a81582a02e62489e6bbd3290f82b8d226ae
Anssh <ansshbprajapati@gmail.com>
2025-07-08 17:44:59 -0700

    n

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
-  base: '/MandirLocator/',
+  base: './',
   plugins: [
     react(),
     runtimeErrorOverlay(),
